34e6971accf643ece32e8687ef3047bc3571672a
vishnumishra <cybercognizant@gmail.com>
2022-01-23 22:17:53 +0530

    Added the sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   imports: [ConfigModule.forRoot({
     envFilePath: '.env',
-  })],
+  }),
+  SequelizeModule.forRoot({
+    dialect: 'mysql',
+    host: 'localhost',
+    port: Number(process.env.MYSQL_PORT),
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    models: [Company, Team],
+  }),
+    CompaniesModule, TeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.PORT)
   const port = process.env.PORT || 3000;
   await app.listen(port);
   console.log(
